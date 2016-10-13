3e0679a19d1efe52145e960c4397447d2ab54bc6
Hugo Garcia <hugoallangm@gmail.com>
2016-10-13 12:19:55 -0600

    Agregando base de datos de defunciones

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -15,4 +15,33 @@
 
 hello <- function() {
   print("Hello, world!")
+}
+
+
+
+suma <- function(a,b){
+  return(a+b)
+}
+
+
+mes_a_numero <- function(data,columna, dia, anio){
+  meses <- ifelse( toupper(data[[columna]])  == "ENERO", 1,
+            ifelse(toupper(data[[columna]])  == "FEBRERO", 2,
+            ifelse(toupper(data[[columna]])  == "MARZO", 3,
+            ifelse(toupper(data[[columna]])  == "ABRIL", 4,
+            ifelse(toupper(data[[columna]])  == "MAYO", 5,
+            ifelse(toupper(data[[columna]])  == "JUNIO", 6,
+            ifelse(toupper(data[[columna]])  == "JULIO", 7,
+            ifelse(toupper(data[[columna]])  == "AGOSTO", 8,
+            ifelse(toupper(data[[columna]])  == "SEPTIEMBRE", 9,
+            ifelse(toupper(data[[columna]])  == "OCTUBRE",10,
+            ifelse(toupper(data[[columna]])  == "NOVIEMBRE",11,
+            ifelse(toupper(data[[columna]])  == "DICIEMBRE", 12, NA))))))))))))
+
+  fecha_completa <- as.Date( paste( data[[ anio ]], meses, data[[dia]], sep = "/"  ) )
+
+  dia_semana <- weekdays(fecha_completa)
+
+
+  return(dia_semana)
 }
